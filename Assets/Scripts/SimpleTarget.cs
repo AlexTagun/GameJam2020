1e232631c4639462aeb2743c6b22e5b36073974f
AlexTagun <alextagun@gmail.com>
2020-01-31 02:25:22 +0300

    FIX IsPointerOverUIObject with touches

--- Assets/Scripts/SimpleTarget.cs
+++ Assets/Scripts/SimpleTarget.cs
@@ -9,7 +9,15 @@ public class SimpleTarget : MonoBehaviour {
     [Inject] private EventManager _eventManager;
 
     private void OnMouseDown() {
-        if (EventSystem.current.IsPointerOverGameObject()) return;
+        if (IsPointerOverUIObject()) return;
         _eventManager.HandleMainMenu();
+    }
+    
+    private bool IsPointerOverUIObject() {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
     }
 }
