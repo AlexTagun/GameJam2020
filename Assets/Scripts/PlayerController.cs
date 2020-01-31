bbbf549b7779337688108db511786d2e1bd0f607
AlexKutsar <alekcandr_123qw@mail.ru>
2020-02-01 02:48:50 +0300

    add pick Item

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private float speedX;
     [SerializeField]
     private float speedY;
-    
+
+    private GameObject pickedItem;
 
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
@@ -24,11 +25,35 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal")*speedX, Input.GetAxisRaw("Vertical")*speedY);
         moveVelocity = moveInput;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(pickedItem);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            Debug.Log("нажмите Е");
+            pickedItem = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Item")
+        {
+            Debug.Log("Подойдите к объекту");
+            pickedItem = null;
+        }
+    }
+
 
 }
