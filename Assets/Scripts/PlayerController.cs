bda2de53e83325018c1b80bd367aa7be130799cf
AlexKutsar <alekcandr_123qw@mail.ru>
2020-02-01 02:23:28 +0300

    add moving player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speedX;
+    [SerializeField]
+    private float speedY;
+    
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal")*speedX, Input.GetAxisRaw("Vertical")*speedY);
+        moveVelocity = moveInput;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
+    }
+
+}
