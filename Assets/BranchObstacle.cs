cfed173b0e099ba977c048ae339465cf946a3815
Vano <iw.lasgin2011@gmail.com>
2020-02-01 07:24:02 +0300

    add branch

---
+++ Assets/BranchObstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchObstacle : MonoBehaviour
+{
+    [SerializeField] private Collider2D BlockCollider;
+    
+    private void OnTriggerStay2D(Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            var playerController = other.gameObject.GetComponent<PlayerController>();
+
+            BlockCollider.enabled = playerController._isGrounded;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other){
+        if (other.gameObject.tag == "Player"){
+            var playerController = other.gameObject.GetComponent<PlayerController>();
+
+            BlockCollider.enabled = true;
+        }
+    }
+}
