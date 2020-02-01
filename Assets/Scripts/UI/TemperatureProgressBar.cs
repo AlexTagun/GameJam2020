da394842be5fa030089dc3d7ebd5bb60668c6524
AlexTagun <alextagun@gmail.com>
2020-02-01 03:26:38 +0300

    delete UI namespase + reformat code

--- Assets/Scripts/UI/TemperatureProgressBar.cs
+++ Assets/Scripts/UI/TemperatureProgressBar.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
-namespace UI
-{
-    public class TemperatureProgressBar : MonoBehaviour
-    {
-        [Inject] private EventManager _eventManager;
+public class TemperatureProgressBar : MonoBehaviour {
+    [Inject] private EventManager _eventManager;
 
-        [SerializeField] private Image FillImage;
+    [SerializeField] private Image FillImage;
 
-        public void SetValue(float currentValue, float maxValue){
-            var fillAmount = currentValue / maxValue;
+    public void SetValue(float currentValue, float maxValue) {
+        var fillAmount = currentValue / maxValue;
 
-            FillImage.fillAmount = fillAmount;
-        }
+        FillImage.fillAmount = fillAmount;
     }
 }
--- Assets/Scripts/UI/UIGameController.cs
+++ Assets/Scripts/UI/UIGameController.cs
@@ -1,29 +1,25 @@
 using UnityEngine;
 using Zenject;
 
-namespace UI
-{
-    public class UIGameController : MonoBehaviour
-    {
-        [Inject] private EventManager _eventManager;
-        [Inject] private TemperatureManager _temperatureManager;
+public class UIGameController : MonoBehaviour {
+    [Inject] private EventManager _eventManager;
+    [Inject] private TemperatureManager _temperatureManager;
 
-        
-        [SerializeField] private TemperatureProgressBar TemperatureProgressBar;
-        [SerializeField] private UITemperaturePanel UiTemperaturePanel;
 
-        private void Awake(){
-            _eventManager.OnGlobalTemperatureChanged += OnGlobalTemperatureChanged;
-            
-            _eventManager.OnPlayerTemperatureChanged += OnPlayerTemperatureChanged;
-        }
+    [SerializeField] private TemperatureProgressBar TemperatureProgressBar;
+    [SerializeField] private UITemperaturePanel UiTemperaturePanel;
 
-        private void OnGlobalTemperatureChanged(float value){
-            UiTemperaturePanel.UpdateView(value);
-        }
+    private void Awake() {
+        _eventManager.OnGlobalTemperatureChanged += OnGlobalTemperatureChanged;
 
-        private void OnPlayerTemperatureChanged(float value){
-            TemperatureProgressBar.SetValue(value, _temperatureManager.MAX_PLAYER_TEMPERATURE);
-        }
+        _eventManager.OnPlayerTemperatureChanged += OnPlayerTemperatureChanged;
+    }
+
+    private void OnGlobalTemperatureChanged(float value) {
+        UiTemperaturePanel.UpdateView(value);
+    }
+
+    private void OnPlayerTemperatureChanged(float value) {
+        TemperatureProgressBar.SetValue(value, _temperatureManager.MAX_PLAYER_TEMPERATURE);
     }
 }
