7c642c774a3d40bdc26a3c2f542c437dea2fe2e0
Vano <iw.lasgin2011@gmail.com>
2020-02-02 02:04:02 +0300

    add exit path

--- Assets/PathController.cs
+++ Assets/PathController.cs
@@ -11,19 +11,27 @@ public class PathController : MonoBehaviour
     
     private void Awake(){
         _eventManager.OnItemPut += OnItemPut;
+        _eventManager.OnItemPicked += OnItemPicked;
     }
     
     private void OnItemPut(){
         var rocket = GameObject.FindWithTag("Rocket").GetComponent<Rocket>();
         var nextItemType = rocket.NextItem;
         
-        UpdatePathStoppers(nextItemType);
+        UpdatePathStoppers(nextItemType, true);
     }
 
-    private void UpdatePathStoppers(TypeItem typeItem){
+    private void OnItemPicked(){
+        var rocket = GameObject.FindWithTag("Rocket").GetComponent<Rocket>();
+        var nextItemType = rocket.NextItem;
+        
+        UpdatePathStoppers(nextItemType, false);
+    }
+
+    private void UpdatePathStoppers(TypeItem typeItem, bool isEnter){
         foreach (var pathStopper in PathStoppers){
             if (pathStopper.TypeItem == typeItem){
-                pathStopper.Hide();
+                pathStopper.Hide(isEnter);
             }
             else{
                 pathStopper.Show();
--- Assets/Scripts/PathStopper.cs
+++ Assets/Scripts/PathStopper.cs
@@ -8,10 +8,17 @@ public class PathStopper : MonoBehaviour
     [SerializeField] private TypeItem _typeItem;
     public TypeItem TypeItem => _typeItem;
     
-    [SerializeField] List<GameObject> Parts = new List<GameObject>();
+    [SerializeField] List<GameObject> EnterParts = new List<GameObject>();
+    
+    [SerializeField] List<GameObject> ExitParts = new List<GameObject>();
 
     public void Show(){
-        foreach (var part in Parts){
+        Show(EnterParts);
+        Show(ExitParts);
+    }
+
+    private void Show(List<GameObject> parts){
+        foreach (var part in parts){
             SpriteRenderer spriteRenderer = part.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null){
                 spriteRenderer.DOFade(1f, 1f);
@@ -24,8 +31,19 @@ public class PathStopper : MonoBehaviour
         }
     }
 
-    public void Hide(){
-        foreach (var part in Parts){
+    public void Hide(bool isEnter){
+        if (isEnter){
+            Hide(EnterParts);
+            Show(ExitParts);
+        }
+        else{
+            Hide(ExitParts);
+            Show(EnterParts);
+        }
+    }
+
+    private void Hide(List<GameObject> parts){
+        foreach (var part in parts){
             SpriteRenderer spriteRenderer = part.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null){
                 spriteRenderer.DOFade(0f, 1f);
