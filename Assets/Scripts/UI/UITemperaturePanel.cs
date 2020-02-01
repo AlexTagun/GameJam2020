ea3fbf77f717c9aafc65a6fae053cdbaf286eb4d
AlexTagun <alextagun@gmail.com>
2020-02-01 03:30:13 +0300

    remove UI namespace in UITemperaturePanel

--- Assets/Scripts/UI/UITemperaturePanel.cs
+++ Assets/Scripts/UI/UITemperaturePanel.cs
@@ -2,17 +2,11 @@ using TMPro;
 using UnityEngine;
 using Zenject;
 
-namespace UI
-{
-    public class UITemperaturePanel : MonoBehaviour
-    {
-
-        [SerializeField] private TextMeshProUGUI Text;
-        
+public class UITemperaturePanel : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI Text;
 
 
-        public void UpdateView(float value){
-            Text.text = value.ToString();
-        }
+    public void UpdateView(float value) {
+        Text.text = value.ToString();
     }
 }
