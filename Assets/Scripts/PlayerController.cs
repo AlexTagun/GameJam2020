078b978a354ed80fe090e27781ccd588c6a91436
AlexTagun <alextagun@gmail.com>
2020-02-01 04:51:17 +0300

    add player prefab + codestyle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField]
-    private float speedX;
-    [SerializeField]
-    private float speedY;
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float speedX;
+    [SerializeField] private float speedY;
 
-    [SerializeField]
-    private Rocket rocket;
+    [SerializeField] private Rocket rocket;
 
     private bool isAbleToPickItem = false;
     private Item itemNearPlayer;
@@ -22,70 +18,55 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveVelocity;
 
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
 
-    void Update()
-    {
+    void Update() {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal") * speedX, Input.GetAxisRaw("Vertical") * speedY);
         moveVelocity = moveInput;
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (isAbleToPickItem)
-            {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (isAbleToPickItem) {
                 typePickedItem = itemNearPlayer.typeItem;
                 itemNearPlayer.PickItem(itemNearPlayer);
                 isAbleToPickItem = false;
             }
 
-            if (isAbleToPutItem)
-            {
-
+            if (isAbleToPutItem) {
                 rocket.PutItem(typePickedItem);
                 isAbleToPutItem = false;
             }
-
         }
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         rb.MovePosition(rb.position + moveVelocity * Time.deltaTime);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Item")
-        {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Item") {
             Debug.Log("нажмите Е, чтобы подобрать");
             isAbleToPickItem = true;
             itemNearPlayer = collision.gameObject.GetComponent<Item>();
         }
-        if (collision.gameObject.tag == "Rocket")
-        {
+
+        if (collision.gameObject.tag == "Rocket") {
             Debug.Log("Нажмите E, чтобы сдать");
             isAbleToPutItem = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Item")
-        {
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Item") {
             Debug.Log("Подойдите к объекту");
             isAbleToPickItem = false;
         }
-        if (collision.gameObject.tag == "Rocket")
-        {
+
+        if (collision.gameObject.tag == "Rocket") {
             Debug.Log("Подойдите к объекту");
             isAbleToPutItem = false;
         }
-
     }
-
-
 }
