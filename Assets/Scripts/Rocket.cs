421f0275d98b84e87e807d544e4fc8f466fcb78d
AlexKutsar <alekcandr_123qw@mail.ru>
2020-02-01 14:09:12 +0300

    add snow slide

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,16 +6,34 @@ using Zenject;
 public class Rocket : MonoBehaviour
 {
     [Inject] private EventManager _eventManager;
-    
+
+    public GameObject snowSlideOnWoodRoad;
+    public GameObject snowSlideOnRockRoad;
+    public GameObject snowSlideOnSandRoad;
      public void PutItem(TypeItem typeItem)
-    {
+     {
         if (typeItem == TypeItem.wood)
+        {
             Debug.Log("Предмет дерево принят");
+            snowSlideOnWoodRoad.SetActive(true);
+        }
         if (typeItem == TypeItem.rock)
+        {
             Debug.Log("Предмет камень принят");
+            snowSlideOnRockRoad.SetActive(true);
+        }
         if (typeItem == TypeItem.sand)
+        {
             Debug.Log("Предмет песок принят");
+            snowSlideOnSandRoad.SetActive(true);
+        }
         
         _eventManager.HandleItemPut();
-    }
+
+     }
+
+    /* void ShowLandSlide()
+    {
+
+    } */
 }
