20e685e372f80efe7a29034036f6a96af79721aa
AlexTagun <alextagun@gmail.com>
2020-02-01 03:08:34 +0300

    FIX nullref in managerInstaller

--- Assets/Scripts/ManagerInstaller.cs
+++ Assets/Scripts/ManagerInstaller.cs
@@ -3,8 +3,7 @@ using Zenject;
 
 public class ManagerInstaller : MonoInstaller {
     public override void InstallBindings() {
-        Container.BindInstance(new EventManager());
-        Container.BindInstance(new TemperatureManager());
-        // Container.BindInstance(gameObject.AddComponent<GameManager>());
+        Container.Bind<EventManager>().AsSingle();
+        Container.Bind<TemperatureManager>().AsSingle();
     }
 }
