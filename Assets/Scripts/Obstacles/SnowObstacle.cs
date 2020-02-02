1a32072bf2a66f790d46c1b49204b02fd556fd3b
AlexTagun <alextagun@gmail.com>
2020-02-02 07:08:14 +0300

    change SNOW_SPEED_MODIFIER

--- Assets/Scripts/Obstacles/SnowObstacle.cs
+++ Assets/Scripts/Obstacles/SnowObstacle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnowObstacle : MonoBehaviour
 {
-    [SerializeField] private readonly float SNOW_SPEED_MODIFIER = 0.1f;
+    [SerializeField] private readonly float SNOW_SPEED_MODIFIER = 0.3f;
     
     private void OnTriggerStay2D(Collider2D other){
         if (other.gameObject.tag == "Player"){
